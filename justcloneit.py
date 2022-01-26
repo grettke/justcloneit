5ef4cf704c6400b67711d944d8be35ef29dc36c6
Grant Rettke <grant@wisdomandwonder.com>
2022-01-25 18:46:09 -0600

    wiC

--- justcloneit.py
+++ justcloneit.py
@@ -1,12 +1,42 @@
-#!/usr/bin/env python
-"""Clone and move an object"""
+# !/usr/bin/env python
 
 import inkex
 
-class JustCloneIt(inkex.TextExtension):
-    """To upper case"""
-    def process_chardata(self, text):
-        return text
+
+class JustCloneIt(inkex.EffectExtension):
+
+  def thisworksright(self):
+    # See Nup.generate_nup().make_clones
+    ideal = inkex.Rectangle(x='50', y='50', width='10', height='10')
+    layer = self.svg.add(inkex.Layer.new('Test Layer'))
+    model = layer.add(ideal.copy())
+    model.set('id', 'testmodel')
+    use = layer.add(inkex.Use())
+    use.set('xlink:href', '#' + 'testmodel')
+    use.transform.add_translate(40, 40)
+
+  def tmp(self):
+    layername = 'bxm.' + datetime.datetime.now().replace(
+      microsecond=0).isoformat()
+    model = inkex.Rectangle(x='50', y='50', width='10', height='10')
+
+    lyr = inkex.Layer.new('a')
+    self.svg.add(lyr)
+
+    ogn = inkex.Rectangle(x='50', y='50', width='10', height='10')
+    ogn.set_id(modelname)
+    lyr.add(ogn)
+
+    ause = inkex.Use()
+    # ause.transform.add_translate(20, 20)
+    # r = inkex.Transform('translate(20, 20)')
+    ause.transform.add_translate(20, 20)
+    ause.set('xlink:href', modelname)
+    lyr.add(ause)
+
+    el1 = inkex.Rectangle(x='10', y='60', width='30', height='20')
+    lyr.add(el1)
+
 
 if __name__ == '__main__':
-    JustCloneIt().run()
+  JustCloneIt().run()
